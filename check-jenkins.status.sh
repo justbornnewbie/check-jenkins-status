f70a5f9a69c29aa4ce2cf3485f44a8128e24cbc2
rjmsas <Rahul.Maurya@connectwise.com>
2021-09-13 17:02:13 -0400

    chaged the persistenet volume

--- check-jenkins.status.sh
+++ check-jenkins.status.sh
@@ -4,8 +4,8 @@ if [[ "$d" != "Up"  ]]
 then
 
         docker ps -a | awk '{ print $1,$2 }' | grep $1 | awk '{print $1 }' | xargs -I {} docker rm -f {}
-        docker run -d -p 8080:8080 -v /opt/jenkins:/var/jenkins_home --name jenkins jenkins/jenkins
+        docker run -d -p 8080:8080 -v /opt/docker/jenkins:/var/jenkins_home --name jenkins jenkins/jenkins
 echo "docker reinstalled"
 else
-        echo "docker service is already up and running"
+        echo "jenkins service is already up and running"
 fi
